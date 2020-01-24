948687812d11a52b77f175821254fc637b5a7ae3
ugursesen <ugursesen@gmail.com>
2020-01-24 20:36:48 +0300

    oxogame

---
+++ oxo/Assets/Scripts/CellController.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class CellController : MonoBehaviour
+{
+    public CellType cellType;
+    public int row, column;
+
+    [SerializeField] private Sprite _markedSprite, _unmarkedSprite;
+    [SerializeField] private Image _imageRef;
+    
+    private GridSpawner _gridSpawnerRef;
+     
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AssignGridSpawnerRef(GridSpawner tempRef)
+    {
+        _gridSpawnerRef = tempRef;
+    }
+    
+    public void CheckPossibleMatch()
+    {
+        CellController[] matches = new CellController[4];
+        
+        int counter=0;
+        
+        if (_gridSpawnerRef.gridArr[row,column].cellType==CellType.Marked)
+        {
+            if (column+1 < _gridSpawnerRef.rowAndColumnSize && _gridSpawnerRef.gridArr[row, column].cellType == _gridSpawnerRef.gridArr[row, column+1].cellType)
+            {
+                matches[counter] = _gridSpawnerRef.gridArr[row, column + 1];
+                counter++;
+                
+            }
+
+            if (column - 1 > 0 && _gridSpawnerRef.gridArr[row, column].cellType == _gridSpawnerRef.gridArr[row, column - 1].cellType)
+            {
+                matches[counter] = _gridSpawnerRef.gridArr[row, column - 1];
+                counter++;
+                
+            }
+
+            if (row + 1 < _gridSpawnerRef.rowAndColumnSize && _gridSpawnerRef.gridArr[row, column].cellType == _gridSpawnerRef.gridArr[row+1, column].cellType)
+            {
+                matches[counter] = _gridSpawnerRef.gridArr[row + 1, column];
+                counter++;
+                
+            }
+
+            if (row - 1 > 0 && _gridSpawnerRef.gridArr[row, column].cellType == _gridSpawnerRef.gridArr[row-1, column].cellType)
+            {
+                matches[counter] = _gridSpawnerRef.gridArr[row - 1, column];
+                counter++;
+                
+            }
+        }
+
+        if (counter>=2)
+        {
+            UnmarkCell();
+
+            for (int i = 0; i < matches.Length; i++)
+            {
+                if (matches[i]!=null)
+                {
+                    matches[i].UnmarkCell();
+                }
+               
+            }
+
+            _gridSpawnerRef.matchCount++;
+
+        }
+ 
+    }
+
+    public void MarkCell()
+    {
+        cellType = CellType.Marked;
+        _imageRef.sprite = _markedSprite;
+    }
+
+    public void UnmarkCell()
+    {
+        cellType = CellType.Unmarked;
+        _imageRef.sprite = _unmarkedSprite;
+    }
+
+    public void CellClicked()
+    {
+        if (cellType==CellType.Unmarked)
+        {
+            MarkCell();
+            _gridSpawnerRef.CheckAllCells();
+        }
+    }
+
+
+    public enum CellType
+    {
+       Unmarked, Marked
+    }
+
+   
+}
---
+++ oxo/Assets/Scripts/GridSpawner.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridSpawner : MonoBehaviour
+{
+    [SerializeField] private InputField _gridSizeIF;
+    [SerializeField] private GridLayoutGroup _gridLayout;
+    [SerializeField] private Transform _gridTransform;
+    [SerializeField] private GameObject _cellPrefab;
+    [SerializeField] private Text _count;
+
+    public CellController[,] gridArr;
+    public int rowAndColumnSize;
+    public int cellCount;
+    public int matchCount;
+
+    public void CreateGrid()
+    {
+        ClearGrid();
+        
+        rowAndColumnSize = int.Parse(_gridSizeIF.text);
+        cellCount = rowAndColumnSize * rowAndColumnSize;
+        
+        _gridLayout.constraintCount = rowAndColumnSize;
+
+        _gridLayout.cellSize = new Vector2(1000 / rowAndColumnSize, 1000 / rowAndColumnSize);
+
+        gridArr = new CellController[rowAndColumnSize,rowAndColumnSize];
+
+        for (int i = 0; i < rowAndColumnSize; i++)
+        {
+            for (int j = 0; j < rowAndColumnSize; j++)
+            {
+                gridArr[i,j] = Instantiate(_cellPrefab, _gridTransform).GetComponent<CellController>();
+                gridArr[i, j].row = i;
+                gridArr[i, j].column = j;
+
+                gridArr[i, j].AssignGridSpawnerRef(this);
+            }
+        }
+    }
+
+    private void ClearGrid()
+    {
+        for (int i = 0; i < _gridTransform.childCount; i++)
+        {
+            Destroy(_gridTransform.GetChild(i).gameObject);
+        }
+    }
+
+    public void CheckAllCells()
+    {
+        for (int i = 0; i < rowAndColumnSize; i++)
+        {
+            for (int j = 0; j < rowAndColumnSize; j++)
+            {
+                gridArr[i, j].CheckPossibleMatch();
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _count.text = "Match Count: " + matchCount.ToString();
+    }
+}
